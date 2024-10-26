034ac87abbfa4c6959545142acf87c39b670a1aa
Calin Cartis <calin.cartis@techstackapps.com>
2024-10-26 19:40:51 +0300

    delete import

--- app/examples/all/page.tsx
+++ app/examples/all/page.tsx
@@ -5,7 +5,6 @@ import styles from "./page.module.css";
 import Chat from "../../components/chat";
 import WeatherWidget from "../../components/weather-widget";
 import { getWeather } from "../../utils/weather";
-import FileViewer from "../../components/file-viewer";
 
 const FunctionCalling = () => {
   const [weatherData, setWeatherData] = useState({});
@@ -43,7 +42,6 @@ const FunctionCalling = () => {
       <div className={styles.container}>
         <div className={styles.column}>
           <WeatherWidget {...weatherData} />
-          <FileViewer />
         </div>
         <div className={styles.chatContainer}>
           <div className={styles.chat}>
