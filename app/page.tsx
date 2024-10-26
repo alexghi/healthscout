62186c13bf8608644f0801b7395a74a2c188d2d3
Alex G <alexg@paymoapp.com>
2024-10-26 16:14:14 +0300

    remove unused files

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ import styles from "./page.module.css";
 
 const Home = () => {
   const categories = {
-    "Basic chat": "basic-chat",
     "Function calling": "function-calling",
-    "File search": "file-search",
     All: "all",
   };
 
