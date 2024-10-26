fce6c3e07f9922ffc2dcd8310e472eb9f5aac886
John Smith <groundmuffin@mail.ru>
2024-10-26 19:53:38 +0300

    clean junk

--- app/examples/function-calling/page.tsx
+++ app/examples/function-calling/page.tsx
@@ -3,26 +3,17 @@
 import React, { useState } from "react";
 import styles from "../shared/page.module.css";
 import Chat from "../../components/chat";
-import WeatherWidget from "../../components/weather-widget";
 import { getWeather } from "../../utils/weather";
 import { getDoctors } from "../../utils/doctors";
 import { RequiredActionFunctionToolCall } from "openai/resources/beta/threads/runs/runs";
 
-interface WeatherData {
-  location?: string;
-  temperature?: number;
-  conditions?: string;
-}
-
 const FunctionCalling = () => {
-  const [weatherData, setWeatherData] = useState<WeatherData>({});
   const [doctorsData, setDoctorsData] = useState([]);
 
   const functionCallHandler = async (call: RequiredActionFunctionToolCall) => {
     // if (call?.function?.name !== "get_weather") return;
     if(call.function.name  === "get_doctors") {
       const data = getDoctors();
-      console.log({data});
       setDoctorsData(data);
       return JSON.stringify(data);
     }
@@ -30,7 +21,6 @@ const FunctionCalling = () => {
     if (call.function.name === "get_weather") {
       const args = JSON.parse(call.function.arguments);
       const data = getWeather(args.location);
-      setWeatherData(data);
       return JSON.stringify(data);
     }
   };
